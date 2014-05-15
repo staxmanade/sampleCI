982bf0a40d888c8c37ac141ed7cbd5188324efec
Jason Jarrett <jason@elegantcode.com>
2014-05-15 11:44:57 -0700

    go

---
+++ test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+describe("something to test", function(){
+
+    it("Should do something cool", function(){
+        assert.equal("hello", "hello");
+    });
+
+    it("Should fail", function(){
+        assert.equal("hello", "NOT");
+    });
+
+});
