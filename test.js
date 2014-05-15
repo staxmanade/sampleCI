d38c13cabcc852ad84df621af411fe69d1349265
Matthew Strickland <matthew@idlefusion.com>
2014-05-15 14:51:55 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ describe("something to test", function(){
     });
 
     it("Should fail", function(){
-        assert.equal("hello", "NOT");
+        assert.equal("hello", "hello");
     });
 
 });
